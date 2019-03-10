84f9fcd7f2a8df60db0bee6356d958b9095b7c93
fosemberg <fosemberg@gmaill.com>
2019-03-10 22:16:34 +0300

    description for todo list

--- src/components/addColorForm/AddColorForm.js
+++ src/components/addColorForm/AddColorForm.js
@@ -1,35 +1,41 @@
 import React from 'react'
-import { Component } from 'react'
+import {Component} from 'react'
 import PropTypes from 'prop-types'
 import './AddColorForm.scss'
 
-const AddColorForm = ({onNewColor=f=>f}) => {
+const AddColorForm = ({onNewColor = f => f}) => {
 
-    let _title, _color
+  let _title, _color
 
-    const submit = e => {
-        e.preventDefault()
-        onNewColor(_title.value, _color.value)
-        _title.value = ''
-        _color.value = '#000000'
-        _title.focus()
-    }
+  const submit = e => {
+    e.preventDefault()
+    onNewColor(_title.value, _color.value)
+    _title.value = ''
+    _color.value = '#000000'
+    _title.focus()
+  }
 
-    return (
-        <form className="add-color" onSubmit={submit}>
-            <input ref={input => _title = input}
-                   type="text"
-                   placeholder="color title..." required/>
-            <input ref={input => _color = input}
-                   type="color" required/>
-            <button>ADD</button>
-        </form>
-    )
+  return (
+    <form className="add-color" onSubmit={submit}>
+      <input ref={input => _title = input}
+             type="text"
+             placeholder="Card title"
+             required
+      />
+      <textarea
+        ref={input => _color = input}
+        placeholder="Card description"
+        rows="4"
+        required
+      />
+      <button>ADD</button>
+    </form>
+  )
 
 }
 
 AddColorForm.propTypes = {
-    onNewColor: PropTypes.func
+  onNewColor: PropTypes.func
 }
 
 export default AddColorForm
--- src/components/colorList/components/color/Color.js
+++ src/components/colorList/components/color/Color.js
@@ -1,46 +1,55 @@
 import React from 'react'
-import { Component } from 'react'
+import {Component} from 'react'
 import PropTypes from 'prop-types'
 import StarRating from './components/starRating/StarRating'
 import TimeAgo from '../../../ui/TimeAgo'
-import { FaTrash } from 'react-icons/fa'
+import {FaTrash, FaCheck} from 'react-icons/fa'
 import './Color.scss'
 
 class Color extends Component {
 
-    render() {
-        const { title, color, rating, timestamp, onRemove, onRate} = this.props
-        return (
-            <section className="color" style={this.style}>
-                <h1 ref="title">{title}</h1>
-                <button onClick={onRemove}>
-                    <FaTrash />
-                </button>
-                <div className="color"
-                     style={{ backgroundColor: color }}>
-                </div>
-                <TimeAgo timestamp={timestamp} />
-                <div>
-                    <StarRating starsSelected={rating} onRate={onRate}/>
-                </div>
-            </section>
-        )
-    }
+  render() {
+    const {title, color, rating, timestamp, onRemove, onRate} = this.props
+    return (
+      <section className="color" style={this.style}>
+        <h1 ref="title">{title}</h1>
+        <button
+          onClick={onRemove}
+          className="check"
+        >
+          <FaCheck/>
+        </button>
+        <button
+          onClick={onRemove}
+          className="remove"
+        >
+          <FaTrash/>
+        </button>
+        <pre className="color">
+                  {color}
+                </pre>
+        <TimeAgo timestamp={timestamp}/>
+        <div>
+          <StarRating starsSelected={rating} onRate={onRate}/>
+        </div>
+      </section>
+    )
+  }
 
 }
 
 Color.propTypes = {
-    title: PropTypes.string.isRequired,
-    color: PropTypes.string.isRequired,
-    rating: PropTypes.number,
-    onRemove: PropTypes.func,
-    onRate: PropTypes.func
+  title: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+  onRemove: PropTypes.func,
+  onRate: PropTypes.func
 }
 
 Color.defaultProps = {
-    rating: 0,
-    onRemove: f=>f,
-    onRate: f=>f
+  rating: 0,
+  onRemove: f => f,
+  onRate: f => f
 }
 
 export default Color
