c6621913431022d835c126ed32f8f326e8e451c6
fosemberg <fosemberg@gmaill.com>
2019-03-10 22:44:47 +0300

    add moving cards from list to list

--- src/actions.js
+++ src/actions.js
@@ -1,30 +1,37 @@
 import C from './constants'
-import { v4 } from 'uuid'
+import {v4} from 'uuid'
 
 export const addColor = (title, color) =>
-    ({
-        type: C.ADD_COLOR,
-        id: v4(),
-        title,
-        color,
-        timestamp: new Date().toString()
-    })
+  ({
+    type: C.ADD_COLOR,
+    id: v4(),
+    title,
+    color,
+    timestamp: new Date().toString(),
+  })
 
 export const removeColor = id =>
-    ({
-        type: C.REMOVE_COLOR,
-        id
-    })
+  ({
+    type: C.REMOVE_COLOR,
+    id,
+  })
 
 export const rateColor = (id, rating) =>
-    ({
-        type: C.RATE_COLOR,
-        id,
-        rating
-    })
+  ({
+    type: C.RATE_COLOR,
+    id,
+    rating,
+  })
+
+export const moveCard = (id, list) =>
+  ({
+    type: C.MOVE_CARD,
+    id,
+    list,
+  })
 
 export const sortColors = sortBy =>
-    ({
-        type: "SORT_COLORS",
-        sortBy
-    })
+  ({
+    type: "SORT_COLORS",
+    sortBy,
+  })
--- src/components/colorList/container.js
+++ src/components/colorList/container.js
@@ -1,20 +1,23 @@
 import connect from 'react-redux/es/connect/connect';
 import {sortFunction} from '../../lib/array-helpers';
-import {rateColor, removeColor} from '../../actions';
+import {rateColor, removeColor, moveCard} from '../../actions';
 import ui from './ColorList';
 
 export const ColorList = connect(
-    state =>
-        ({
-            colors: [...state.colors].sort(sortFunction(state.sort))
-        }),
-    dispatch =>
-        ({
-            onRemove(id) {
-                dispatch(removeColor(id))
-            },
-            onRate(id, rating) {
-                dispatch(rateColor(id, rating))
-            }
-        })
+  state =>
+    ({
+      colors: [...state.colors].sort(sortFunction(state.sort))
+    }),
+  dispatch =>
+    ({
+      onRemove(id) {
+        dispatch(removeColor(id))
+      },
+      onRate(id, rating) {
+        dispatch(rateColor(id, rating))
+      },
+      onMove(id, list) {
+        dispatch(moveCard(id, list))
+      }
+    })
 )(ui)
