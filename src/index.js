38da8e923f2c7dc3b3e269e4de04b6b81b0cc74e
fosemberg <fosemberg@gmaill.com>
2019-03-11 05:59:17 +0300

    add version to console

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
+import {render} from 'react-dom'
+import {Provider} from 'react-redux'
 import App from './components/App'
 import storeFactory from './store'
 
 const store = storeFactory()
 
+console.log('version:', '0.1.0')
+
 window.React = React
 window.store = store
 
 render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
 )
