f12dd068408d2dcbe963b797092d2006bfbe22d2
fosemberg <fosemberg@gmaill.com>
2019-03-11 05:14:46 +0300

    Updates

---
+++ precache-manifest.a30335167f89a8622235e5e9527bbe59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c5b0f0a8e9c81c578fb",
+    "url": "/todo-list/static/css/main.d0ab5ea8.chunk.css"
+  },
+  {
+    "revision": "8c5b0f0a8e9c81c578fb",
+    "url": "/todo-list/static/js/main.c84e8059.chunk.js"
+  },
+  {
+    "revision": "86494e9c6d32fb67be2b",
+    "url": "/todo-list/static/js/runtime~main.714397e2.js"
+  },
+  {
+    "revision": "5916bcbc9e7146fde870",
+    "url": "/todo-list/static/js/2.19b77a41.chunk.js"
+  },
+  {
+    "revision": "0c6a10535f69caad0fe8fe5ccae38799",
+    "url": "/todo-list/index.html"
+  }
+];
